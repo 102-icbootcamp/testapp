5a76565061a430efba2744110cc3632412c70caf
Yasser Mahmud <yasser@programifyjs.com>
2019-01-20 13:08:00 +0500

    initial3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ class Home extends React.Component {
           <div class="card checkup-card">
             <img src="img/checkup.jpg" class="card-img checkup-img" alt="checkup" />
             <div class="card-img-overlay checkup-overlay">
-              <p class="card-text checkup-text">Are you ready to book your free Check - Up appointment?</p>
+              <p class="card-text checkup-text">Are you ready to book your free Check - Up appointment?????</p>
               <p class="card-text">
                 <a href="tel:000"><button class="checkup-btn checkup-btn-phone"><span class="checkup-icon"><i class="fas fa-phone"></i></span>Talk to us now</button></a>
                 <Link to="/contact"><button class="checkup-btn checkup-btn-desktop"><span class="checkup-icon"><i class="fas fa-phone"></i></span>Talk to us now</button></Link>
